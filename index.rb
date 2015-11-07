15c00afab2072e3e5595b4c6ef8a31894a71a41f
Jorge Díaz <jorgedf@gmail.com>
2015-11-07 15:07:42 +0200

    Add validation action and views.

--- index.rb
+++ index.rb
@@ -9,6 +9,27 @@ require 'sequel'
 require 'nokogiri'
 
 require 'pry'
+
+SCHEMAS_DIR = "./schemas/"
+SCHEMAS_VERSIONS = {'v113-p15' => "Version 1.1.3-patch 15 (Oct 2015)"}
 get '/' do
+  haml :index, :format => :html5
+end
+
+post '/validate' do
+  if !params[:xml_body].nil? && !params[:version].nil?
+    @message = params[:xml_body]
+    doc = Nokogiri::XML(@message)
+    @ndc_message = doc.root.name
+    schemas_path = SCHEMAS_DIR + params[:version]
+    Dir.chdir(schemas_path) do
+      schema_file = "#{@ndc_message}.xsd"
+      puts "Loading NDC Schema... (#{schema_file})"
+      xsd = Nokogiri::XML::Schema(File.read(schema_file))
+      @errors = xsd.validate(doc)
+    end
+  else
+    @errors = "UNDEFINED ERROR while processing request"
+  end
   haml :index, :format => :html5
 end
