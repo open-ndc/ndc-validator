51f9c30e52cd53f546013ec8e6370dc7411776fc
Jorge Díaz <jorgedf@gmail.com>
2015-11-10 09:40:54 +0100

    Controller light refactoring.

--- index.rb
+++ index.rb
@@ -1,13 +1,20 @@
-require "bundler/setup"
-require "sinatra"
+require 'bundler/setup'
+require 'sinatra'
 require 'sinatra/partial'
 require 'pry'
 
 require 'haml'
-require "net/http"
+require 'net/http'
+# require 'sequel'
 require 'nokogiri'
+require './core_ext/object'
 
 require 'pry'
+
+module NDCValidator
+  class IvalidNDCMessageError < RuntimeError; end
+  class NDCParseUndefinedError < RuntimeError; end
+end
 
 SCHEMAS_DIR = "./schemas/"
 SCHEMAS_VERSIONS = {'v113-p15-2' => "Version 1.1.3 - patch 15.2 (Oct 2015)"}
@@ -16,19 +23,27 @@ get '/' do
 end
 
 post '/validate' do
-  if !params[:xml_body].nil? && !params[:version].nil?
+  @errors = []
+  if params[:xml_body].present? || !params[:version].present?
     @message = params[:xml_body]
     doc = Nokogiri::XML(@message)
-    @ndc_message = doc.root.name
-    schemas_path = SCHEMAS_DIR + params[:version]
-    Dir.chdir(schemas_path) do
-      schema_file = "#{@ndc_message}.xsd"
-      puts "Loading NDC Schema... (#{schema_file})"
-      xsd = Nokogiri::XML::Schema(File.read(schema_file))
-      @errors = xsd.validate(doc)
+    if doc.root
+      @ndc_message = doc.root.name
+      if !@ndc_message.empty?
+        schemas_path = SCHEMAS_DIR + params[:version]
+        Dir.chdir(schemas_path) do
+          schema_file = "#{@ndc_message}.xsd"
+          xsd = Nokogiri::XML::Schema(File.read(schema_file))
+          @errors = xsd.validate(doc)
+        end
+      else
+        @errors << NDCValidator::IvalidNDCMessageError.new("Invalid/unknown NDC method")
+      end
+    else
+      @errors << NDCValidator::NDCParseUndefinedError.new("Unsuccessful message parsing")
     end
   else
-    @errors = "UNDEFINED ERROR while processing request"
+    @errors << RuntimeError.new("Invalid/empty Message/Version")
   end
   haml :index, :format => :html5
 end
