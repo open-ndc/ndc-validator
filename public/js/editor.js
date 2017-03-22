117a0f8423b71fd4ffe0a3fa405b2dcd23007213
Matias Insaurralde <matias@insaurral.de>
2017-03-22 02:19:41 -0300

    Use ACE editor

--- public/js/editor.js
+++ public/js/editor.js
@@ -2,16 +2,19 @@
 console.log("editor.js loaded!");
 
 window.onload = function(e){
-  var textAreaEditor = document.getElementById('xml_body');
-  if (textAreaEditor !== null) {
-    var codeMirror = CodeMirror.fromTextArea(textAreaEditor, {
-      value: textAreaEditor.value,
-      mode: 'application/xml',
-      tabMode: 'indent',
-      lineNumbers: true,
-      autofocus: true
-    });
-  } else {
-    alert("ERR: textAreaEditor not found in document!")
-  }
+  var editor = ace.edit('xml_body')
+  editor.setOptions({
+    minLines: 30,
+    maxLines: 50
+  })
+  editor.getSession().setMode("ace/mode/xml")
+
+  $('form').submit(function(e) {
+    var contents = editor.getValue()
+    var elem = $('<input type="hidden" name="xml_body" value=""/>')
+    elem.val(contents)
+    $('form').append(elem)
+    e.submit()
+    e.preventDefault()
+  })
 };
